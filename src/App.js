cd40cc293c62f728a8809f06ada39b5ace78a551
mashimaro2010 <amnat.kaewtunkum@gmail.com>
2023-12-06 14:39:06 +0700

    fix Bug state start Open Form

--- src/App.js
+++ src/App.js
@@ -2,62 +2,62 @@ import "./App.css";
 import Header from "./components/Header";
 import AddForm from "./components/AddForm";
 import Item from "./components/Item";
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
 
 function App() {
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
-  const [title, setTitle] = useState("");
-  const [editId, setEditId] = useState("null");
-  const [theme,setTheme]=useState("dark");
+  const [tasks,setTasks]=useState(JSON.parse(localStorage.getItem("tasks"))||[])
+  const [title,setTitle] = useState("")
+  const [editId,setEditId] = useState(null);
+  const [theme,setTheme] = useState("light");
   //รูปแบบ 3
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+  },[tasks])
 
-  function deleteTask(id) {
-    const result = tasks.filter((item) => item.id !== id);
-    setTasks(result);
+  function deleteTask(id){
+    const result = tasks.filter(item=>item.id !==id)
+    setTasks(result)
   }
-  function editTask(id) {
+  function editTask(id){
     //1
-    setEditId(id);
-    const editTask = tasks.find((item) => item.id === id);
-    setTitle(editTask.title);
+    setEditId(id)
+    const editTask = tasks.find((item)=>item.id === id)
+    setTitle(editTask.title)
   }
-  function saveTask(e) {
+  function saveTask(e){
     e.preventDefault();
-    if (!title) {
+    if(!title){
       alert("กรุณาป้อนข้อมูลด้วยครับ")
-    } else if (editId) {
+    }else if(editId){
       //อัพเดตข้อมูล
-      const updateTask = tasks.map((item) => {
+      const updateTask = tasks.map((item)=>{
         //รายการใดมีรหัสตรงกับรหัสแก้ไข
-        if (item.id === editId) {
-          return { ...item, title: title }
+        if(item.id === editId){
+          return {...item,title:title}
         }
         return item;
       })
       setTasks(updateTask)
       setEditId(null)
       setTitle("")
-    } else {
+    }else{
       //เพิ่มรายการใหม่
-      const newTask = {
-        id: Math.floor(Math.random() * 1000),
-        title: title
+      const newTask={
+        id:Math.floor(Math.random()*1000),
+        title:title
       }
-      setTasks([...tasks, newTask])
+      setTasks([...tasks,newTask])
       setTitle("")
     }
   }
   return (
     <div className={"App "+theme}>
-      <Header theme={theme} setTheme={setTheme} />
+      <Header theme={theme} setTheme={setTheme}/>
       <div className="container">
         <AddForm title={title} setTitle={setTitle} saveTask={saveTask} editId={editId}/>
         <section>
           {
-            tasks.map((data) => (
+            tasks.map((data)=>(
             <Item key={data.id} data={data} deleteTask={deleteTask} editTask={editTask}/>
           ))
         }
